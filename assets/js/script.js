a0398c35c7c377a328096c7a1b8ef72de6b2da08
Chrotesque <github@chrotesque.com>
2022-02-17 23:12:56 +0000

    html/js: changeDifficulty function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    let buttons = document.getElementsByTagName("button");
-    for (let button of buttons) {
+    let buttonsAll = document.getElementsByTagName("button");
+    for (let button of buttonsAll) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "help" || this.getAttribute("data-type") === "settings") {
-                let menu = this.getAttribute("data-type");
-                toggleMenu(menu);
+            if (this.getAttribute("data-type") === "menu") {
+                toggleMenu(this, this.getAttribute("data-value"));
+            } else if (this.getAttribute("data-type") === "difficulty") {
+                changeDifficulty(buttonsAll, this.getAttribute("data-value"));
+            } else if (this.getAttribute("data-type") === "game-control") {
+                controlGame(this);
             }
         })
     }
-
 })
 
-function toggleMenu(menu) {
 
-    let div = document.getElementById("menu-" + menu);
+/**
+ * Showing | Hiding menus through adding &| removing css class
+ * @param {object} button 
+ * @param {string} selection 
+ */
+function toggleMenu(button, selection) {
+
+    let div = document.getElementById("menu-" + selection);
     if (div.className === "hide-element") {
+        button.className = "selected";
         div.className = "show-element";
     } else {
         div.className = "hide-element";
+        button.removeAttribute("class");
+    }
+
+}
+
+/**
+ * Change game difficulty for next game through adding &| removing css class
+ * @param {object} buttons 
+ * @param {string} selection 
+ */
+function changeDifficulty(buttons, selection) {
+
+    console.log(typeof (selection));
+    for (let button of buttons) {
+        if (button.getAttribute("data-type") === "difficulty" && button.getAttribute("data-value") === selection) {
+            button.className = "selected";
+        } else {
+            button.removeAttribute("class");
+        }
     }
 
 }
